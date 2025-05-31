2a04c5f47b533c2bbe2e530e628581265c2a3d2e
Jayadeep KM <kmjayadeep@gmail.com>
2025-06-01 00:00:35 +0200

    fix kubeseal
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- pkg/kubeseal/seal.go
+++ pkg/kubeseal/seal.go
@@ -7,15 +7,22 @@ import (
 
 	Z "github.com/rwxrob/bonzai"
 	"github.com/rwxrob/bonzai/cmds/help"
+	"github.com/rwxrob/bonzai/persisters/inprops"
+	"github.com/rwxrob/bonzai/term"
 	"github.com/rwxrob/bonzai/vars"
 )
 
+var props = inprops.NewUserCache("x", "kubeseal.props")
+
 var Cmd = &Z.Cmd{
 	Name:    `kubeseal`,
-	Short: `Kubeseal helper commands`,
+	Short: `kubeseal helper commands`,
+	Alias: `seal`,
+	Usage: `kubeseal [cert-path]`,
 	Long: `Helper commands instead of kubeseal.
      https://github.com/bitnami-labs/sealed-secrets/
-     Set cert path using 'x kubeseal var set kubeseal-cert <path>'`,
+     Cert path is cached after first use`,
+	Def: sealCmd,
 	Cmds: []*Z.Cmd{help.Cmd, sealCmd, vars.Cmd},
 }
 
@@ -24,7 +31,16 @@ var sealCmd = &Z.Cmd{
 	Short:  `Seal given secret`,
 	Cmds: []*Z.Cmd{help.Cmd},
 	Do: func(x *Z.Cmd, args ...string) error {
-		cert := x.Caller().Get(`kubeseal-cert`)
+		if len(args) > 0 {
+			props.Set(`kubeseal-cert`, args[0])
+		}
+
+		cert := props.Get(`kubeseal-cert`)
+		if cert == "" {
+			_ ,err := term.Print(`No cert path provided. Please provide the path to the kubeseal certificate.`)
+			return err
+		}
+
 		cmd := exec.Command("kubeseal", "--format=yaml", "--cert="+cert)
 		cmd.Stdin = os.Stdin
 		cmd.Stderr = os.Stderr
